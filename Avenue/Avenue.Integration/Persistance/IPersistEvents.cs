3dcb78a502936d772361e0dc12d5ace70f0e4bde
beolson <beolson@gmail.com>
2012-05-02 22:12:49 -0500

    stuff

---
+++ Avenue/Avenue.Integration/Persistance/IPersistEvents.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Avenue.Integration.Persistance
+{
+    interface IPersistEvents
+    {
+        IEnumerable<string> GetEventsRelatedToMessage(Guid messageId, int skip, int take);
+    }
+}
---
+++ Avenue/Avenue.Integration/Persistance/IPersistMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Avenue.Integration.Persistance
+{
+    public interface IPersistMessage
+    {
+        //get recent messages
+
+        //Get 
+
+        IEnumerable<ApplicationBus.Message> GetMessagesByLocalId(Guid messageId, int skip, int limit);
+
+        IEnumerable<ApplicationBus.Message> GetMessagesLike(string search, int skip, int limit);
+
+        void SaveMessage(ApplicationBus.Message message);
+
+     //   IEnumerable<string> GetEventsRelatedToMessage(Guid messageId, int skip, int take);
+
+        //Handle commited and uncommited messages
+
+    }
+}
